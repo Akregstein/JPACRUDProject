64243174400a9d2a9d284fb0d708d958f9b53c38
Daniel.Kregstein <Akregstein@hotmail.com>
2022-04-15 19:07:28 -0600

    stub entity tests

---
+++ JPACountries/src/test/java/com/skilldistillery/countries/entities/CountryTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.countries.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CountryTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Country country;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPACountries");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		country = em.find(Country.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		country = null;
+	}
+
+	@Test
+	void test_Country_Entity_Mapping() {
+		assertNotNull(country);
+		assertEquals("Norway", country.getName());
+	}
+
+}
