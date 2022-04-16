6a3fb9d6235c3b754a214e0db846fc940f9a4b6b
Daniel.Kregstein <Akregstein@hotmail.com>
2022-04-15 19:30:58 -0600

    Start Boot MVC project, add JPA dependency

---
+++ TravelBucketList/src/main/java/com/skilldistillery/countries/TravelBucketListApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBucketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBucketListApplication.class, args);
+	}
+
+}
